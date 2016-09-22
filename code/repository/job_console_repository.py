19f83c9346c279331fa4d0bcda44d5aea00df573
Wei TC Liu <shlw@cn.ibm.com>
2016-09-22 09:05:35 +0800

    add utf-8 output in console

--- code/repository/job_console_repository.py
+++ code/repository/job_console_repository.py
@@ -1,6 +1,13 @@
+import json
+from bson import json_util
 
+def date_handler(obj):
+    if hasattr(obj, 'isoformat'):
+        return obj.isoformat()
+    else:
+        raise obj
 
 class JobConsoleRepository(object):
 
 	def add_job(self, job):
-		print(str(job))
+		print(json.dumps(job, ensure_ascii=False, default=date_handler))
