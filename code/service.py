ab643ccb09919bc9327eb3ab900a4dc740f9f921
Wei TC Liu <shlw@cn.ibm.com>
2016-09-22 08:54:03 +0800

    modify the help output

--- code/service.py
+++ code/service.py
@@ -43,7 +43,7 @@ def get_output_channel(argv):
                 print ("Options:")
                 print ("   %-30s%s" % ("-o [ --output ]", "channel to save the crawling results, the output channel can be:"))
                 print ("   %-30s  %-10s%s" % ("", "console", "print the result in cosole"))
-                print ("   %-30s  %-10s%s" % ("", "mongodb", "save the result to mongodb database"))
+                print ("   %-30s  %-10s%s" % ("", "mongodb", "save the result to the mongodb database"))
                 print ("   %-30s%s" % ("", "and the default channel is [console]"))
                 
                 print ("   %-30s%s" % ("-h [ --help ]", "show this usage information"))
